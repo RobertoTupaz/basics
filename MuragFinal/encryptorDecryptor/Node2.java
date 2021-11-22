2cf8ef7f6322a629e159ab1e3bcc77b2ba3f5a23
RobertoTupaz <79069387+RobertoTupaz@users.noreply.github.com>
2021-11-22 20:10:24 +0800

    Add files via upload

---
+++ MuragFinal/encryptorDecryptor/Node2.java
@@ -0,0 +1,17 @@
+package encryptorDecryptor;
+
+public class Node2 {
+	Node2 next;
+	Node2 previous;
+	
+	String data1;
+	String data2;
+	
+	public String getData1() {
+		return ("inputed    : " + data1);
+	}
+	
+	public String getData2() {
+		return ("decrypted : " + data2);
+	}
+}
